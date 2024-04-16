a7e60272feba351090ee71b811d51978427e40cc
. <.>
2024-04-16 15:15:17 -0500

    Agregando el viaje de julian

---
+++ tallerJs/2-workshop/el-viaje-de-julian.js
@@ -0,0 +1,18 @@
+
+
+const destino = prompt('Ingrese el destino');
+const estadia = prompt('Precio de la estadia diaria');
+let alimentacion = prompt('ingrese cuanto vale la alimentacion diaria en dolar');
+
+let presupuesto = prompt('Ingrese presupuesto');
+let dias = prompt('Ingrese cantidad de dias');
+
+
+
+if (presupuesto > (dias * (estadia + alimentacion))) {
+    alert('Julian tiene suficiente dinero');
+} else {
+    alert('Ajustar el presupuesto o la duracion del viaje');
+}
+
+

e9b39bceeccf9daf3eaf179de142c52e0dc6286f
Christian Pabon <christian75876@gmail.com>
2024-04-17 15:46:26 -0500

    Add Reto 3: Decisiones de Última Hora

--- tallerJs/2-workshop/desisiones-ultima-hora.js
+++ tallerJs/2-workshop/desisiones-ultima-hora.js
@@ -2,8 +2,8 @@
 const articlesRain = [
     article1 = {
         name: 'paraguas',
-        weigth: 1.5,
-        size: 1.0
+        weigth: 3,
+        size: 1
     },
     article2 = {
         name: 'botas de lluvia',
@@ -12,7 +12,7 @@ const articlesRain = [
     },
     article3 = {
         name: 'chaqueta',
-        weight: 2.5,
+        weight: 2,
         size: 3,
     }
 ];
@@ -20,8 +20,8 @@ const articlesRain = [
 const articlesSum = [
     article1 = {
         name: 'bloqueador',
-        weigth: 0.5,
-        size: 0.5
+        weight: 2,
+        size: 2,
     },
     article2 = {
         name: 'gafas',
@@ -30,28 +30,61 @@ const articlesSum = [
     },
     article3 = {
         name: 'gorra',
-        weight: 1.5,
+        weight: 3,
         size: 2,
     }
 ];
 
-const articlesCount = (articles)=>{
-    articles.forEach(article =>{
+const listArticles = [];
+
+const articlesCount = (articles) => {
+    articles.forEach(article => {
         alert(`Este es uno de los articulos que puedes llevar dado el clima: ${article.name}`);
-       });
-       return
+    });
+    return articles;
 }
+
+const addArticles = (option, aux,weightBag ,weight,spaceBag) => {
+    while (option && weightBag < weight && spaceBag >= 0) {
+        let article = parseInt(prompt('Escoge el numero del indice del aticulo que deseas agregar'));
+        listArticles.push(aux[article]);
+        weightBag += aux[article].weight;
+        spaceBag -= aux[article].size;
+        console.log(weightBag);
+        option = confirm('Deseas agregar alguno de esto articulos?');
+    }
+    if(weightBag > weight || spaceBag <= 0){
+        alert('As alcanzado el limite permitido');
+    }
+
+    return listArticles;
+}
+
 
 let climate = parseInt(prompt('si el cima es soleado ingresa: 0\nsi el clima es con lluvia ingresa: 1 '));
 
-let spaceBag = parseFloat(prompt('Ingresa el espacio disponible en tu maleta. Si tienes la mitad disponible, ingresa 5.0 Si tienes disponible una cuarta parte, ingresa 2.5'));
-let weightBag = parseInt(prompt('Cuanto pesa tu maleta actualmente?'))
+let spaceBag = parseFloat(prompt('Ingresa cuantos compartimientos tienes?'));
+let weightBag = parseInt(prompt('Cuanto pesa tu maleta actualmente?'));
 
-let weight = parseInt(prompt('Ingresa cuanto el es peso permitido de la maleta?'))
+let weight = parseInt(prompt('Ingresa cuanto el es peso permitido para la maleta?'));
 
-if (climate === 0){
-   let sum = articlesCount(articlesSum);
-}if (climate === 1) {
-    let rain = articlesCount(articlesRain);
+if (climate === 0 && !isNaN(climate)) {
+    let aux = articlesCount(articlesSum);
+    console.table(aux);
+    let option = confirm('Deseas agregar alguno de esto articulos?');
+    let result = addArticles(option, aux, weightBag, weight, spaceBag);
+    alert(`Sus articulos agregados son: revise consola`)
+    console.table(listArticles);
+
+} if (climate === 1 && !isNaN(climate)) {
+    aux = articlesCount(articlesRain);
+    option = confirm('Deseas agregar alguno de esto articulos?');
+    console.table(aux);
+    result = addArticles(option, aux, weightBag, weight, spaceBag);
+    alert(`Sus articulos agregados son: revise consola`)
+    console.table(listArticles);
 }
+
+
+
 
